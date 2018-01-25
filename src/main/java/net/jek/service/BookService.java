dd2edafd7c56b3ea26078f298e82bd185cdf1b27
blighter7 <blighter7@gmail.com>
2018-01-25 13:46:06 +0200

    First commit

---
+++ src/main/java/net/jek/service/BookService.java
@@ -0,0 +1,12 @@
+package net.jek.service;
+
+import net.jek.model.Book;
+import java.util.List;
+
+public interface BookService {
+    void addBook(Book book);
+    void updateBook(Book book);
+    void removeBook(int id);
+    Book getBookById(int id);
+    List<Book> listBooks();
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,13 @@
+
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Jek$</title>
+  </head>
+  <body>
+  <h3>Jek presents</h3>
+  <br/>
+  <a href="/books" target="_blank">Books list</a>
+  <br/>
+  </body>
+</html>
